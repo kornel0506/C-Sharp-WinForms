e8a94b4f491b32fcee6e77ace7ed7bea75e0345a
korian <106070773+kornel0506@users.noreply.github.com>
2024-07-02 12:02:47 +0200

    Add files via upload

---
+++ Uszoverseny/Uszoverseny/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace Uszoverseny
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstVersenyzok = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtRajtszam = new System.Windows.Forms.TextBox();
+            this.txtOrszag = new System.Windows.Forms.TextBox();
+            this.txtIdoEredmeny = new System.Windows.Forms.TextBox();
+            this.txtEletKor = new System.Windows.Forms.TextBox();
+            this.btnGyoztes = new System.Windows.Forms.Button();
+            this.txtGyoztesIdo = new System.Windows.Forms.TextBox();
+            this.btnBezar = new System.Windows.Forms.Button();
+            this.btnAdatBe = new System.Windows.Forms.Button();
+            this.rchTxtGyoztes = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // lstVersenyzok
+            // 
+            this.lstVersenyzok.FormattingEnabled = true;
+            this.lstVersenyzok.Location = new System.Drawing.Point(25, 99);
+            this.lstVersenyzok.Name = "lstVersenyzok";
+            this.lstVersenyzok.Size = new System.Drawing.Size(172, 290);
+            this.lstVersenyzok.Sorted = true;
+            this.lstVersenyzok.TabIndex = 0;
+            this.lstVersenyzok.SelectedIndexChanged += new System.EventHandler(this.lstVersenyzok_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.Brown;
+            this.label1.Location = new System.Drawing.Point(128, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "200 m-es pillangó úszás";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(61, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Résztvevők";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(228, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Rajtszám:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(231, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Ország:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(231, 185);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Időeredmény:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(234, 228);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Életkor:";
+            // 
+            // txtRajtszam
+            // 
+            this.txtRajtszam.Location = new System.Drawing.Point(319, 96);
+            this.txtRajtszam.Name = "txtRajtszam";
+            this.txtRajtszam.ReadOnly = true;
+            this.txtRajtszam.Size = new System.Drawing.Size(100, 20);
+            this.txtRajtszam.TabIndex = 8;
+            // 
+            // txtOrszag
+            // 
+            this.txtOrszag.Location = new System.Drawing.Point(319, 139);
+            this.txtOrszag.Name = "txtOrszag";
+            this.txtOrszag.ReadOnly = true;
+            this.txtOrszag.Size = new System.Drawing.Size(100, 20);
+            this.txtOrszag.TabIndex = 9;
+            // 
+            // txtIdoEredmeny
+            // 
+            this.txtIdoEredmeny.Location = new System.Drawing.Point(319, 182);
+            this.txtIdoEredmeny.Name = "txtIdoEredmeny";
+            this.txtIdoEredmeny.ReadOnly = true;
+            this.txtIdoEredmeny.Size = new System.Drawing.Size(100, 20);
+            this.txtIdoEredmeny.TabIndex = 10;
+            // 
+            // txtEletKor
+            // 
+            this.txtEletKor.Location = new System.Drawing.Point(319, 225);
+            this.txtEletKor.Name = "txtEletKor";
+            this.txtEletKor.ReadOnly = true;
+            this.txtEletKor.Size = new System.Drawing.Size(100, 20);
+            this.txtEletKor.TabIndex = 11;
+            // 
+            // btnGyoztes
+            // 
+            this.btnGyoztes.Enabled = false;
+            this.btnGyoztes.Location = new System.Drawing.Point(231, 291);
+            this.btnGyoztes.Name = "btnGyoztes";
+            this.btnGyoztes.Size = new System.Drawing.Size(75, 23);
+            this.btnGyoztes.TabIndex = 12;
+            this.btnGyoztes.Text = "Győztes";
+            this.btnGyoztes.UseVisualStyleBackColor = true;
+            this.btnGyoztes.Click += new System.EventHandler(this.btnGyoztes_Click);
+            // 
+            // txtGyoztesIdo
+            // 
+            this.txtGyoztesIdo.Location = new System.Drawing.Point(319, 293);
+            this.txtGyoztesIdo.Name = "txtGyoztesIdo";
+            this.txtGyoztesIdo.ReadOnly = true;
+            this.txtGyoztesIdo.Size = new System.Drawing.Size(100, 20);
+            this.txtGyoztesIdo.TabIndex = 13;
+            // 
+            // btnBezar
+            // 
+            this.btnBezar.Location = new System.Drawing.Point(257, 413);
+            this.btnBezar.Name = "btnBezar";
+            this.btnBezar.Size = new System.Drawing.Size(138, 23);
+            this.btnBezar.TabIndex = 14;
+            this.btnBezar.Text = "Bezár";
+            this.btnBezar.UseVisualStyleBackColor = true;
+            this.btnBezar.Click += new System.EventHandler(this.btnBezar_Click);
+            // 
+            // btnAdatBe
+            // 
+            this.btnAdatBe.Location = new System.Drawing.Point(48, 413);
+            this.btnAdatBe.Name = "btnAdatBe";
+            this.btnAdatBe.Size = new System.Drawing.Size(127, 23);
+            this.btnAdatBe.TabIndex = 15;
+            this.btnAdatBe.Text = "Adatok beolvasása";
+            this.btnAdatBe.UseVisualStyleBackColor = true;
+            this.btnAdatBe.Click += new System.EventHandler(this.btnAdatBe_Click);
+            // 
+            // rchTxtGyoztes
+            // 
+            this.rchTxtGyoztes.Location = new System.Drawing.Point(231, 320);
+            this.rchTxtGyoztes.Name = "rchTxtGyoztes";
+            this.rchTxtGyoztes.Size = new System.Drawing.Size(188, 69);
+            this.rchTxtGyoztes.TabIndex = 16;
+            this.rchTxtGyoztes.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Bisque;
+            this.ClientSize = new System.Drawing.Size(458, 476);
+            this.Controls.Add(this.rchTxtGyoztes);
+            this.Controls.Add(this.btnAdatBe);
+            this.Controls.Add(this.btnBezar);
+            this.Controls.Add(this.txtGyoztesIdo);
+            this.Controls.Add(this.btnGyoztes);
+            this.Controls.Add(this.txtEletKor);
+            this.Controls.Add(this.txtIdoEredmeny);
+            this.Controls.Add(this.txtOrszag);
+            this.Controls.Add(this.txtRajtszam);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstVersenyzok);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Úszóverseny";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstVersenyzok;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtRajtszam;
+        private System.Windows.Forms.TextBox txtOrszag;
+        private System.Windows.Forms.TextBox txtIdoEredmeny;
+        private System.Windows.Forms.Button btnGyoztes;
+        private System.Windows.Forms.TextBox txtGyoztesIdo;
+        private System.Windows.Forms.Button btnBezar;
+        private System.Windows.Forms.Button btnAdatBe;
+        private System.Windows.Forms.RichTextBox rchTxtGyoztes;
+        private System.Windows.Forms.TextBox txtEletKor;
+    }
+}
+
---
+++ Uszoverseny/Uszoverseny/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uszoverseny
+{
+    public partial class Form1 : Form
+    {
+        List<Versenyzo> versenyzok = new List<Versenyzo>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AdatBeolvasas()
+        {
+            StreamReader sr = new StreamReader("uszok.txt");
+
+            string adat = sr.ReadLine();
+            while (adat != null)
+            {
+                Feldolgoz(adat);
+                adat = sr.ReadLine();
+            }
+            sr.Close();
+        }
+
+        private void Feldolgoz(string adat)
+        {
+            string rajtSzam, nev, orszag;
+            DateTime szulDatum;
+            TimeSpan idoEredmeny;
+            Versenyzo versenyzo;
+            string[] t = adat.Split(';'); 
+
+            rajtSzam = t[0];
+            nev = t[1];
+            szulDatum = DateTime.Parse(t[2]);
+            orszag = t[3];
+            idoEredmeny = TimeSpan.Parse("0:" + t[4]);
+            versenyzo = new Versenyzo(rajtSzam, nev, szulDatum, orszag, idoEredmeny);
+            lstVersenyzok.Items.Add(versenyzo);
+            versenyzok.Add(versenyzo);
+        }
+
+        private void btnAdatBe_Click(object sender, EventArgs e)
+        {
+            lstVersenyzok.Items.Clear();
+            AdatBeolvasas();
+            btnAdatBe.Enabled = false;
+            btnGyoztes.Enabled = true;
+        }
+
+        private void lstVersenyzok_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Versenyzo versenyzo = versenyzok[lstVersenyzok.SelectedIndex];
+            txtRajtszam.Text = versenyzo.Rajtszam;
+            txtOrszag.Text = versenyzo.Orszag;
+            txtIdoEredmeny.Text = new DateTime(versenyzo.IdoEredmeny.Ticks).ToString("mm:ss:ff");
+            txtEletKor.Text = (DateTime.Now.Year - versenyzo.SzulDatum.Year) + " év";
+        }
+
+        private void btnGyoztes_Click(object sender, EventArgs e)
+        {
+            TimeSpan min = (lstVersenyzok.Items[0] as Versenyzo).IdoEredmeny;
+            foreach(var item in lstVersenyzok.Items)
+            {
+                if ((item as Versenyzo).IdoEredmeny < min)
+                {
+                    min = (item as Versenyzo).IdoEredmeny;
+                }
+            }
+            txtGyoztesIdo.Text = new DateTime(min.Ticks).ToString("mm:ss:ff");
+            rchTxtGyoztes.Clear();
+            foreach(var item in lstVersenyzok.Items)
+            {
+                if ((item as Versenyzo).IdoEredmeny == min)
+                {
+                    rchTxtGyoztes.AppendText(item + "\n");
+                }
+            }
+        }
+
+        private void btnBezar_Click(object sender, EventArgs e)
+        {
+            DialogResult valasz = MessageBox.Show("Biztosan kilép?", "Megerősítés", MessageBoxButtons.YesNo);
+            if (valasz == DialogResult.Yes) this.Close();
+        }
+    }
+
+    class Versenyzo
+    {
+        public string Rajtszam { get; private set; }
+        public string Nev { get; private set; }
+        public DateTime SzulDatum { get; private set; }
+        public string Orszag { get; private set; }
+        public TimeSpan IdoEredmeny { get; private set; }
+
+        public Versenyzo(string rajtszam, string nev, DateTime szulDatum, string orszag, TimeSpan idoEredmeny)
+        {
+            this.Rajtszam = rajtszam;
+            this.Nev = nev;
+            this.SzulDatum = szulDatum;
+            this.Orszag = orszag;
+            this.IdoEredmeny = idoEredmeny;
+        }
+
+        public override string ToString()
+        {
+            return $"{Rajtszam}: {Nev}";
+        }
+    }
+}
