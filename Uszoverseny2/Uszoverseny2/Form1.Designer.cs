c41dd8885fcc77edc84d662cdbf8b439f1c8ea89
korian <106070773+kornel0506@users.noreply.github.com>
2024-07-08 12:24:48 +0200

    Add files via upload

---
+++ Uszoverseny2/Uszoverseny2/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Uszoverseny2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fájlToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.megnyitasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mentesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kilepesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();  
+            this.nevjegyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+
+
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fájlToolStripMenuItem,
+            this.nevjegyToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(656, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // FileToolStripMenuItem
+            // 
+            this.fájlToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.megnyitasToolStripMenuItem,
+            this.kilepesToolStripMenuItem});
+            this.fájlToolStripMenuItem.Name = "FileToolStripMenuItem";
+            this.fájlToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fájlToolStripMenuItem.Text = "Fájl";
+            // 
+            // OpenToolStripMenuItem
+            // 
+            this.megnyitasToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("OpenToolStripMenuItem.Image")));
+            this.megnyitasToolStripMenuItem.Name = "OpenToolStripMenuItem";
+            this.megnyitasToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.megnyitasToolStripMenuItem.Text = "Megnyitás     Ctrl+M";
+            this.megnyitasToolStripMenuItem.Click += new System.EventHandler(this.megnyitasToolStripMenuItem_Click);
+            // 
+            // SaveToolStripMenuItem
+            // 
+            this.mentesToolStripMenuItem.Name = "SaveToolStripMenuItem";
+            this.mentesToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.mentesToolStripMenuItem.Text = "Mentés           Ctrl+S";
+            // 
+            // ExitToolStripMenuItem
+            // 
+            this.kilepesToolStripMenuItem.Name = "ExitToolStripMenuItem";
+            this.kilepesToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.kilepesToolStripMenuItem.Text = "Kilépés            Alt+F4";
+            // 
+            // névjegyToolStripMenuItem
+            // 
+            this.nevjegyToolStripMenuItem.Name = "névjegyToolStripMenuItem";
+            this.nevjegyToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.nevjegyToolStripMenuItem.Text = "Névjegy";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Nyertes kocsma";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+            #region Windows Form Designer generated code
+
+            /// <summary>
+            /// Required method for Designer support - do not modify
+            /// the contents of this method with the code editor.
+            /// </summary>
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fájlToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem megnyitasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mentesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kilepesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem versenyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eredmenyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nevjegyToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Uszoverseny2/Uszoverseny2/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uszoverseny2
+{
+    public partial class Form1 : Form
+    {
+        private List<Versenyzo> versenyzok = new List<Versenyzo>();
+        private VersenyForm versenyForm = new VersenyForm();
+        private EredmenyForm eredmenyForm = new EredmenyForm();
+        private void megnyitasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader sr = null;
+                try
+                {
+                    string fajlNev = openFileDialog1.FileName;
+                    sr = new StreamReader(fajlNev);
+
+                    AdatBevitel(sr);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "hibaüzenet a fejlesztő számára");
+                }
+                finally
+                {
+                    if (sr != null)
+                    {
+                        sr.Close();
+                    }
+                }
+            }
+        }
+
+        private void AdatBevitel(StreamReader sr)
+        {
+            string sor;
+            string[] tordelt;
+            string nev, orszag, zaszlo;
+
+            while (!sr.EndOfStream)
+            {
+                sor = sr.ReadLine();
+                if (sor != "")
+                {
+                    tordelt = sor.Split(';');
+                    nev = tordelt[0];
+                    orszag = tordelt[1];
+                    zaszlo = tordelt[2];
+                    versenyzok.Add(new Versenyzo(nev, orszag, zaszlo));
+                }
+            }
+        }
+    }
+
+    class Versenyzo
+    {
+        public string Rajtszam { get; private set; }
+        public string Nev { get; private set; }
+        public string OrszagNev { get; private set; }
+        public string ZaszloNev { get; private set; }
+        public TimeSpan IdoEredmeny { get; private set; }
+
+        private static int sorSzam;
+
+        public Versenyzo(string nev, string orszagNev, string zaszloNev)
+        {
+            this.Nev = nev;
+            this.OrszagNev = orszagNev;
+            this.ZaszloNev = zaszloNev;
+            sorSzam++;
+            this.Rajtszam = (sorSzam < 10)?("0" + sorSzam): sorSzam.ToString();
+        }
+
+        public override string ToString()
+        {
+            return $"{Nev}";
+        }
+
+        public void Versenyez(TimeSpan idoEredmeny)
+        {
+            this.IdoEredmeny = idoEredmeny;
+        }
+    }
+}
